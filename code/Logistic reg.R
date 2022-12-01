204c1a300442d5feb16e95d41ba6bc70ec07d7ff
Yizhi_Liang <yzl.aca@gmail.com>
2022-12-01 11:04:45 -0500

    update change to the correct dataset 11:04 am Dec 1 2022

--- code/Logistic reg.R
+++ code/Logistic reg.R
@@ -8,19 +8,13 @@ source(here("code","epicalc_v3.R"))
 
 #Load data
 # evansData<-rio::import("data/evans_example_dat.csv", header=T)
-mi_onset_10 <- import(here("data","MI_Onset_10.rdata"))
-mi_onset_10 <- mi_onset_10 %>% mutate(sedendm = case_when(
-  phys_activity==0 & dm==1 ~ 1,
-  phys_activity!=0 & dm==1 ~ 2,
-  phys_activity==0 & dm!=1 ~ 3,
-  phys_activity!=0 & dm!=1 ~ 4
-))
+cvdeath <- import(here("data","CVdeath_Case_control.rdata"))
 
 # adjust factor variables
-fac_vars <- names(mi_onset_10)[c(1,3:10,12:14)]
-mi_onset_10[fac_vars] <- lapply(mi_onset_10[fac_vars],factor)
+fac_vars <- names(cvdeath)[c(3:10)]
+cvdeath[fac_vars] <- lapply(cvdeath[fac_vars],factor)
 
-mi_onset_10_cat <- mi_onset_10 %>%
+cvdeath_cat <- cvdeath %>%
   mutate(age_cat = fct_recode(age_cat,
                               "<50yrs"="1","50-64yrs"="2","65+yrs"="3"),
          female_cat=fct_recode(female,"F"="1","M"="0"),
@@ -29,38 +23,29 @@ mi_onset_10_cat <- mi_onset_10 %>%
          dm_cat=fct_recode(dm,"yes"="1","no"="0"),
          htn_cat=fct_recode(htn,"yes"="1","no"="0"),
          phys_cat=fct_recode(phys_activity,"<1/wk"="0","1-3/wk"="1","4+/wk"="2"),
-         evermarj_cat=fct_recode(evermarj,"yes"="1","no"="0"),
-         dead_cat=fct_recode(dead,"yes"="1","no"="0"),
-         cvdeath_cat=fct_recode(cvdeath,"CVD death"="1","not CVD death"="0"),
-         sedentary = fct_recode(phys_activity, "yes"="0", "no"="1","no"="2"),
-         sedendm_cat = fct_recode(sedendm, "sedentary and diabetes"="1",
-                                  "no sedentary and diabetes"="2", "sedentary and no diabetes"="3",
-                                  "no sedentary and no diabetes"="4")
-  ) %>%
-  mutate(dead_cat = fct_rev(dead_cat),
-         sedentary = fct_rev(sedentary),
-         sedendm_cat = fct_rev(sedendm_cat))
+         case_cat = fct_recode(case, "case"="1","non-case"="0")
+  )
 
 # overview the dataset
-skimr::skim(mi_onset_10_cat)
+skimr::skim(cvdeath_cat)
 
 # 2. 2*2 table
-(crosstable <- mi_onset_10_cat %>%
+(crosstable <- cvdeath_cat %>%
   group_by(dm_cat) %>%
-  count(cvdeath_cat) %>%
+  count(case_cat) %>%
   mutate(prop = n/sum(n)))
 crosstable[4,3]*crosstable[1,3]/(crosstable[2,3]*crosstable[3,3])
 
 # 3. create a logit regre
-log_1 <- glm(cvdeath_cat ~ dm_cat,
-             data = mi_onset_10_cat,
+log_1 <- glm(case_cat ~ dm_cat,
+             data = cvdeath_cat,
              family = binomial(link = "logit"))
 (log_1 %>% tidy(conf.int=T))
 (log_1 %>% tidy(exponentiate=T,conf.int=T))
 
 # 5 cvdeath~dm+age
-log_2 <- glm(cvdeath_cat ~ dm_cat + age,
-             data = mi_onset_10_cat, family = binomial(link = "logit"))
+log_2 <- glm(case_cat ~ dm_cat + age,
+             data = cvdeath_cat, family = binomial(link = "logit"))
 (log_2 %>%  tidy(conf.int=T))
 (log_2 %>% tidy(exponentiate=T, conf.int=T))
 
@@ -68,8 +53,8 @@ log_2 <- glm(cvdeath_cat ~ dm_cat + age,
 exp(10*(log_2 %>%  tidy(conf.int=T))[3,2])
 
 # 7 cvdeath~dm+age+htn+female+phys_activity+educ
-log_3 <- glm(cvdeath_cat ~ dm_cat+age+htn_cat+female_cat+phys_cat+educ_cat,
-             data = mi_onset_10_cat,
+log_3 <- glm(case_cat ~ dm_cat+age+htn_cat+female_cat+phys_cat+educ_cat,
+             data = cvdeath_cat,
              family = binomial(link = "logit"))
 (log_3 %>%  tidy(conf.int=T))
 (log_3 %>% tidy(exponentiate=T, conf.int=T))
@@ -79,10 +64,14 @@ modelsummary(list(log_1, log_2, log_3),
              stars = T,
              output = here("weekly materials","6 Logistic regression",
                            "log_1 to log_3.docx"))
+modelsummary(list(log_1, log_2, log_3),
+             exponentiate = T,
+             stars = T,
+             output = "huxtable")
 
 # 8 htn*dm
-log_4 <- glm(cvdeath_cat ~ dm_cat*htn_cat+age+female_cat+phys_cat+educ_cat,
-             data = mi_onset_10_cat,
+log_4 <- glm(case_cat ~ dm_cat*htn_cat+age+female_cat+phys_cat+educ_cat,
+             data = cvdeath_cat,
              family = binomial(link = "logit"))
 modelsummary(list(log_3, log_4),
              estimate  = "{estimate} ({conf.low}, {conf.high})",
@@ -90,5 +79,10 @@ modelsummary(list(log_3, log_4),
              exponentiate = T,
              output = here("weekly materials","6 Logistic regression",
                                             "log_3 and 4.docx"))
+modelsummary(list(log_3,log_4),
+             estimate  = "{estimate} ({conf.low}, {conf.high})",
+             exponentiate = T,
+             statistic = NULL,
+             output = "huxtable")
 
 
